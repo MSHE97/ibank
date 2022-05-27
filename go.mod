991424b7f339e8248de718e0a1b5bb9129f28003
SherMSH <sherzod.mustafokulov@gmail.com>
2022-05-27 19:05:02 +0500

    Types module connected

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/MSHE97/ibank
+
+go 1.17
+
+require github.com/MSHE97/types v0.0.0-20220527135216-d13f54bd9e57
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/MSHE97/types/pkg/types"
+)
+
+func main() {
+	fmt.Println(types.Euro)
+}
